470c9608cc671f2cc6210d0cbb5d9e54be99a097
GustavoSantosRodrigues <s.rodrigues.2050@hotmail.com>
2026-03-04 16:28:49 -0300

    chore: add fastify swagger generation

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,71 @@
 import 'dotenv/config'
 
 import Fastify from 'fastify'
-import { ZodTypeProvider, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { ZodTypeProvider, jsonSchemaTransform, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import z from 'zod'
+
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
 
 
 const app = Fastify({
-  logger: true
+    logger: true
 })
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.withTypeProvider<ZodTypeProvider>().route({
- method: 'GET',
- url: '/',
- schema: {
-    description: 'hello world',
-    tags: ['hello world'],
-  response: {
-    200: z.object({
-      message: z.string(),
-    
-    })
-   }
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Bootcamp Treinos API',
+      description: 'Api para todos os treinos do bootcamp',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8081',
+        description: 'Local server'
+      }
+    ],
   },
- handler: async function handler (request, reply) {
-  return { message: 'hello world' }
- }
+  transform: jsonSchemaTransform,
+
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+});
+
+const LOGIN_SCHEMA = z.object({
+  username: z.string().max(32).describe('Some description for username'),
+  password: z.string().max(32),
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+    method: 'GET',
+    url: '/',
+    schema: {
+        description: 'hello world',
+        tags: ['hello world'],
+        response: {
+            200: z.object({
+                message: z.string(),
+
+            })
+        }
+    },
+    handler: async function handler(request, reply) {
+        return { message: 'hello world' }
+    }
 });
 
 
 app.listen({ port: 4949 });
 
 try {
-  await app.listen({ port: parseInt(process.env.PORT || '3000') })
+    await app.listen({ port: parseInt(process.env.PORT || '3000') })
 } catch (err) {
-  app.log.error(err)
-  process.exit(1)
+    app.log.error(err)
+    process.exit(1)
 }
